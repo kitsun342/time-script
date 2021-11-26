c0439786d67ea70d2074c5a51f2fa472676502a8
mitti3 <89256593+kitsun342@users.noreply.github.com>
2021-11-26 12:43:21 +0500

    time-script

---
+++ script.js
@@ -0,0 +1,80 @@
+var time = prompt('Введите число от 0 до 24:')
+
+time = Number(time)
+
+switch(time){
+  case 1:
+    alert('Час ночи')
+    break
+  case 2:
+    alert('Два часа ночи')
+    break
+  case 3:
+    alert('Три часа ночи')
+    break
+  case 4:
+    alert('Четыре часа ночи')
+    break
+  case 5:
+    alert('Пять утра')
+    break
+  case 6:
+    alert('Шесть утра')
+    break
+  case 7:
+    alert('Семь утра')
+    break
+  case 8:
+    alert('Восемь утра')
+    break
+  case 9:
+    alert('Девять утра')
+    break
+  case 10:
+    alert('Десять утра')
+    break
+  case 11:
+    alert('Одиннадцать утра')
+    break
+  case 12:
+    alert('Полдень')
+    break
+  case 13:
+    alert('Час дня')
+    break
+  case 14:
+    alert('Два часа дня')
+    break
+  case 15:
+    alert('Три часа дня')
+    break
+  case 16:
+    alert('Четыре часа дня')
+    break
+  case 17:
+    alert('Пять часов дня')
+    break
+  case 18:
+    alert('Шесть часов вечера')
+    break
+  case 19:
+    alert('Семь часов вечера')
+    break
+  case 20:
+    alert('Восемь часов вечера')
+    break
+  case 21:
+    alert('Девять часов вечера')
+    break
+  case 22:
+    alert('Десять часов вечера')
+    break
+  case 23:
+    alert('Одиннадцать часов вечера')
+    break
+  case 24:
+    alert('Полночь')
+    break
+  default:
+    alert('Вы ввели неправильное число')
+}
